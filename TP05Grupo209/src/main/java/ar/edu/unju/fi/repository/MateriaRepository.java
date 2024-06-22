6e4827d79c510e852aa1878c5780e518c531f91a
diego <diego@DiegoHA>
2024-06-22 01:20:51 -0300

    MateriaDTO resuelto - Aguirre Diego

--- TP05Grupo209/src/main/java/ar/edu/unju/fi/repository/MateriaRepository.java
+++ TP05Grupo209/src/main/java/ar/edu/unju/fi/repository/MateriaRepository.java
@@ -1,13 +1,17 @@
 package ar.edu.unju.fi.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+
 import ar.edu.unju.fi.model.Materia;
 
 @Repository
-public interface MateriaRepository extends JpaRepository<Materia, String> {
+public interface MateriaRepository extends JpaRepository<Materia, Long> {
 	List<Materia> findByEstado(boolean estado);
+	@Override
+	Optional<Materia> findById(Long id);
 }

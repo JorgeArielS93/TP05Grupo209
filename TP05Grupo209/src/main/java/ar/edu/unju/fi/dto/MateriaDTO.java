9a28477154f8c7683632602fa412a0e2fad7aed2
diego <diego@DiegoHA.mshome.net>
2024-07-03 18:09:37 -0300

    Correcciones de errores en el formMateria - Aguirre Diego

--- TP05Grupo209/src/main/java/ar/edu/unju/fi/dto/MateriaDTO.java
+++ TP05Grupo209/src/main/java/ar/edu/unju/fi/dto/MateriaDTO.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Component;
 
 import ar.edu.unju.fi.model.Carrera;
 import ar.edu.unju.fi.model.Docente;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -13,6 +15,8 @@ import lombok.NoArgsConstructor;
 @Component
 public class MateriaDTO {
 	private Long codigo;
+	@NotBlank(message = "Debe ingresar el DNI")
+	@Size(min=6, max=40, message = "El nombre de la carrera debe contener entre 6 y 40 caracteres")
 	private String nombre;
 	private String curso;
 	private int cantidadHoras;

a626c070f09c25da92843a2eed875106910a3e4b
diego <diego@DiegoHA.mshome.net>
2024-07-03 21:50:59 -0300

    Correcciones en formMateria - Aguirre Diego

--- TP05Grupo209/src/main/java/ar/edu/unju/fi/controller/MateriaController.java
+++ TP05Grupo209/src/main/java/ar/edu/unju/fi/controller/MateriaController.java
@@ -1,5 +1,8 @@
 package ar.edu.unju.fi.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,55 +12,82 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import ar.edu.unju.fi.dto.DocenteDTO;
 import ar.edu.unju.fi.dto.MateriaDTO;
+import ar.edu.unju.fi.service.ICarreraService;
+import ar.edu.unju.fi.service.IDocenteService;
 import ar.edu.unju.fi.service.IMateriaService;
 
 @Controller
 @RequestMapping("/materia")
 public class MateriaController {
-	 	@Autowired
-	    MateriaDTO materiaDTO;
-	 	
-	 	@Autowired
-		IMateriaService materiaService;
+	@Autowired
+    MateriaDTO materiaDTO;
+ 	
+ 	@Autowired
+ 	DocenteDTO docente;
+ 	
+ 	@Autowired
+	IMateriaService materiaService;
+ 	
+ 	@Autowired
+ 	ICarreraService carreraService;
+ 	
+ 	@Autowired
+ 	IDocenteService docenteService;
 
-	    @GetMapping("formularioMateria")
-	    public ModelAndView getFormulario() {
-	        ModelAndView mv = new ModelAndView("formMateria");
-	        mv.addObject("materia", materiaDTO);
-	        mv.addObject("isEdit", false);
-	        return mv;
-	    }
+    @GetMapping("formularioMateria")
+    public ModelAndView getFormulario() {
+        ModelAndView mv = new ModelAndView("formMateria");
+        mv.addObject("materia", materiaDTO);
+        mv.addObject("isEdit", false);
+        mv.addObject("docentes", docenteService.getListaDocentes());
+        mv.addObject("carreras", carreraService.getListaCarreras());
+        mv.addObject("materias", materiaService.getListaMaterias());
+        return mv;
+    }
 
-	    @GetMapping("listadoMaterias")
-	    public ModelAndView getListaMaterias() {
-	        ModelAndView mv = new ModelAndView("listadoMaterias");
-	        mv.addObject("materias", materiaService.getListaMaterias());
-	        return mv;
-	    }
+    @GetMapping("listadoMaterias")
+    public ModelAndView getListaMaterias() {
+        ModelAndView mv = new ModelAndView("listadoMaterias");
+        mv.addObject("materias", materiaService.getListaMaterias());
+        mv.addObject("docentes", docenteService.getListaDocentes());
+        mv.addObject("carreras", carreraService.getListaCarreras());
+        return mv;
+    }
 
-	    @PostMapping("guardarMateria")
-	    public ModelAndView guardarMateria(@ModelAttribute("materia") MateriaDTO materiaDTO) {
-	        if (materiaDTO.getCodigo() != null) {
-	            materiaService.actualizarMateria(materiaDTO);
-	        } else {
-	            materiaService.agregarUnaMateria(materiaDTO);
-	        }
-	        return new ModelAndView("redirect:listadoMaterias");
-	    }
+    @PostMapping("guardarMateria")
+    public ModelAndView guardarMateria(@ModelAttribute("materia") MateriaDTO materiaDTO) {
+		/*
+		 * docente =
+		 * docenteService.findDocenteByLegajo(materiaDTO.getDocente().getLegajo());
+		 * materiaDTO.setDocente(docente);
+		 */
+    	if (materiaDTO.getCodigo() != null) {
+            materiaService.actualizarMateria(materiaDTO);
+        } else {
+            materiaService.agregarUnaMateria(materiaDTO);
+        }
+        return new ModelAndView("redirect:listadoMaterias");
+    }
+    
+    
+    
+    @GetMapping("/modificar/{codigo}")
+    public ModelAndView modificarMateria(@PathVariable("codigo") Long codigo) {
+        ModelAndView mv = new ModelAndView("formMateria");
+        mv.addObject("docentes", docenteService.getListaDocentes());
+        mv.addObject("carreras", carreraService.getListaCarreras());
+        mv.addObject("materia", materiaService.findMateriaByCodigo(codigo));
+        mv.addObject("isEdit", true);
+        return mv;
+    }
 
-	    @GetMapping("/modificar/{codigo}")
-	    public ModelAndView modificarMateria(@PathVariable("codigo") Long codigo) {
-	        ModelAndView mv = new ModelAndView("formMateria");
-	        mv.addObject("materia", materiaService.findMateriaByCodigo(codigo));
-	        mv.addObject("isEdit", true);
-	        return mv;
-	    }
+    @GetMapping("/borrarMateria/{codigo}")
+    public ModelAndView deleteMateria(@PathVariable("codigo") Long codigo) {
+        materiaService.eliminarUnaMateria(codigo);
+        return new ModelAndView("redirect:/materia/listadoMaterias");
+    }
 
-	    @GetMapping("/borrarMateria/{codigo}")
-	    public ModelAndView deleteMateria(@PathVariable("codigo") Long codigo) {
-	        materiaService.eliminarUnaMateria(codigo);
-	        return new ModelAndView("redirect:/materia/listadoMaterias");
-	    }
 
 }
--- TP05Grupo209/src/main/java/ar/edu/unju/fi/dto/MateriaDTO.java
+++ TP05Grupo209/src/main/java/ar/edu/unju/fi/dto/MateriaDTO.java
@@ -2,11 +2,14 @@ package ar.edu.unju.fi.dto;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import ar.edu.unju.fi.model.Alumno;
 import ar.edu.unju.fi.model.Carrera;
 import ar.edu.unju.fi.model.Docente;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -15,12 +18,15 @@ import lombok.NoArgsConstructor;
 @Component
 public class MateriaDTO {
 	private Long codigo;
+	@NotBlank(message = "Debe ingresar el DNI")
+	@Size(min=6, max=40, message = "El nombre de la carrera debe contener entre 6 y 40 caracteres")
 	private String nombre;
 	private String curso;
 	private int cantidadHoras;
 	private String modalidad;
 	private boolean estado;
-	private List<Alumno> alumnos;
-	private Carrera carrera;
+	@Autowired
 	private Docente docente;
+	@Autowired
+	private Carrera carrera;
 }
